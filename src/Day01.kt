660b96a9fb9c3968d7a835cfc06f3ba5c98342c9
Scholar17 <thawzinoojanuary@gmail.com>
2022-12-19 16:45:31 +0630

    solve puzzle for aoc day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,33 @@
+import java.io.File
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun parseInput(input: String): List<List<Int>> {
+       return input.split("\n\r").map { elf ->
+            elf.lines().map{
+                it.toIntOrNull() ?: 0
+            }
+        }
+    }
+    fun part1(input: String): Int {
+        val data: List<List<Int>> = parseInput(input)
+        return data.maxOf{
+            it.sum()
+        }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: String): Int {
+        val data = parseInput(input)
+        return data.map {
+            it.sum()
+        }.sortedDescending().take(3).sum()
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = File("src/Day01_test.txt").readText()
+//    check(part1(testInput) == 24000)
 
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+//    val input = readInput("Day01")
+    part1(testInput).println()
+    part2(testInput).println()
+//    part2(input).println()
 }
